681dfb389a4a3ed5d9541b5156d3a16ce6f1c1b9
lijunsong <554856005@qq.com>
2019-03-25 15:45:00 +0800

    Java程序性能提升

--- src/main/java/effectivejava/CH2/javatuning/ch2/flyweight/Main.java
+++ src/main/java/effectivejava/CH2/javatuning/ch2/flyweight/Main.java
@@ -2,9 +2,9 @@ package effectivejava.CH2.javatuning.ch2.flyweight;
 
 public class Main {
 
-	public static void main(String[] args) {
-		ReportManagerFactory rmf=new ReportManagerFactory();
-		IReportManager rm=rmf.getFinancialReportManager("A");
-		System.out.println(rm.createReport());
-	}
+    public static void main(String[] args) {
+        ReportManagerFactory rmf = new ReportManagerFactory();
+        IReportManager rm = rmf.getFinancialReportManager("A");
+        System.out.println(rm.createReport());
+    }
 }
--- src/main/java/effectivejava/CH2/javatuning/ch2/flyweight/ReportManagerFactory.java
+++ src/main/java/effectivejava/CH2/javatuning/ch2/flyweight/ReportManagerFactory.java
@@ -4,25 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReportManagerFactory {
-	
-	Map<String ,IReportManager> financialReportManager=new HashMap<String ,IReportManager>();
-	Map<String ,IReportManager> employeeReportManager=new HashMap<String ,IReportManager>();
-	
-	IReportManager getFinancialReportManager(String tenantId){
-		IReportManager r=financialReportManager.get(tenantId);
-		if(r==null){
-			r=new FinancialReportManager(tenantId);
-			financialReportManager.put(tenantId, r);
-		}
-		return r;
-	}
-	
-	IReportManager getEmployeeReportReportManager(String tenantId){
-		IReportManager r=employeeReportManager.get(tenantId);
-		if(r==null){
-			r=new EmployeeReportManager(tenantId);
-			employeeReportManager.put(tenantId, r);
-		}
-		return r;
-	}
+
+    Map<String, IReportManager> financialReportManager = new HashMap<String, IReportManager>();
+    Map<String, IReportManager> employeeReportManager = new HashMap<String, IReportManager>();
+
+    //初始化一次即可，如果值变化，还得考虑更新
+    IReportManager getFinancialReportManager(String tenantId) {
+        IReportManager r = financialReportManager.get(tenantId);
+        if (r == null) {
+            r = new FinancialReportManager(tenantId);
+            financialReportManager.put(tenantId, r);
+        }
+        return r;
+    }
+
+    IReportManager getEmployeeReportReportManager(String tenantId) {
+        IReportManager r = employeeReportManager.get(tenantId);
+        if (r == null) {
+            r = new EmployeeReportManager(tenantId);
+            employeeReportManager.put(tenantId, r);
+        }
+        return r;
+    }
 }
